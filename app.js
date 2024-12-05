c07377404c3fa6154607c5c934e0175400c559f9
GUL5522 <@gmail.com>
2024-12-05 09:58:07 +0530

    intial commit

---
+++ app.js
@@ -0,0 +1,5 @@
+// this is clock
+const parts = [...document.querySelectorAll('#hour,#minute,#seconds')]
+setInterval(() => {
+ new Date().toTimeString().split(' ')[0].split(':').forEach( (part, i) => parts[i].textContent = part)
+}, 1000)
